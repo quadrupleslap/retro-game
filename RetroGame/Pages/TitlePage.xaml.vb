5b0a4afaf77e7692038d4e68a7f031285909da99
Ram Kaniyur <quadrupleslap@gmail.com>
2016-10-01 07:51:24 +1000

    Added quit button.

--- RetroGame/Pages/TitlePage.xaml.vb
+++ RetroGame/Pages/TitlePage.xaml.vb
@@ -4,6 +4,10 @@
         Windows.Application.Current.Shutdown()
     End Sub
 
+    Private Sub Quit()
+        Windows.Application.Current.Shutdown()
+    End Sub
+
     Private Sub TitlePage_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         PlayTrack(Track.Menu)
     End Sub
